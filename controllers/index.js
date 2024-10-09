b0606f6834ece7c6b57578d95853fdaeab04c834
Kimberly Nelson <129911891+gypze@users.noreply.github.com>
2024-10-09 18:49:54 -0500

    update w/ walt

---
+++ controllers/index.js
@@ -0,0 +1,4 @@
+const router = require('express').Router();
+//const userRoutes = require('./userRoutes');
+//const reviewRoutes = require('./reviewRoutes');
+router.get('/', (req, res) => { res.send('Hello World!') });
