1980c1c0268d9162a570ad1b71084a7ef522b788
Kimberly Nelson <129911891+gypze@users.noreply.github.com>
2024-10-09 21:06:10 -0500

    updated files

--- controllers/index.js
+++ controllers/index.js
@@ -2,3 +2,20 @@ const router = require('express').Router();
 //const userRoutes = require('./userRoutes');
 //const reviewRoutes = require('./reviewRoutes');
 router.get('/', (req, res) => { res.send('Hello World!') });
+
+
+const bookController = require('./bookController');
+
+router.use('/books', bookController);
+
+
+const reviewController = require('./reviewController');
+
+router.use('/reviews', reviewController);
+
+
+const userController = require('./userController');
+
+router.use('/users', userController);
+
+module.exports = router;
