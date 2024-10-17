f4b414e5ddc6e66e2407cde01b32c0f5cb08787c
Walter Perry <walterjamesperry@gmail.com>
2024-10-17 17:09:40 -0500

    feat: implement book search functionality with new API integration; update UI for search results

--- controllers/bookSearchRoutes.js
+++ controllers/bookSearchRoutes.js
@@ -49,4 +49,20 @@ router.get("/edit/:id", withGuard, async (req, res) => {
   }
 });
 
+// this is not using withGuard or apiGuard
+router.post("/", async (req, res) => {
+  try {
+    const books = await fetch(
+      "https://www.googleapis.com/books/v1/volumes?q=" +
+        req.body.searchterm +
+        "&maxResults=10"
+    );
+    const bookdata = await books.json();
+
+    res.json(bookdata.items);
+  } catch (error) {
+    res.json(error);
+  }
+});
+
 module.exports = router;
